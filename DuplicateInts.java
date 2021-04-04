e00590b73b023274fa3a21d0a960588cf9a96327
Sean Campbell <sean.campbell3161@gmail.com>
2021-04-04 18:00:58 -0500

    first commit

---
+++ DuplicateInts.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2021 Sean Campbell
+ */
+
+/**
+ *
+ * @author sean
+ */
+
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+*/
+public class DuplicateInts {
+    
+    public boolean containsDuplicate(int[] nums) {
+        
+        Arrays.sort(nums);
+        
+        if (nums.length > 1) {
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] == nums[i-1]) {
+                    return true;
+                }
+            }
+        } 
+        
+        return false;
+        
+    }
+    
+}
