922908ae1a39994e51a9dbb94a9c30ff3dd61947
Sean Campbell <sean.campbell3161@gmail.com>
2021-05-09 18:02:44 -0500

    first commit

---
+++ PlayingOnAChessboard.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2021 Sean Campbell
+ */
+
+/**
+ *
+ * @author sean
+ */
+public class PlayingOnAChessboard {
+    
+    public static String game(long n) {
+        
+        
+        
+    }
+    
+}
