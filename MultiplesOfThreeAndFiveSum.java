dd2a4a6411e83226638f3ac626f5c50dc40e6996
Sean Campbell <sean.campbell3161@gmail.com>
2021-05-27 12:21:25 -0500

    find the sum of multiples of 3 and 5 below value

---
+++ MultiplesOfThreeAndFiveSum.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sean
+ */
+
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we 
+get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 
+below the number passed in.
+*/
+public class MultiplesOfThreeAndFiveSum {
+    
+    public static int solution(int number) {
+    int sum=0;
+    
+    for (int i=0; i < number; i++){
+      if (i%3==0 || i%5==0){sum+=i;}
+    }
+    return sum;
+  }
+    
+    
+    public static void main(String[] args) {
+        System.out.println(solution(25));
+    }
+}
